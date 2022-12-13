b33dd2a0cc0e1595b746b4f0c8b8a781a03efbb5
iobaidu5 <i.obaidu5@gmail.com>
2022-12-13 22:07:15 +0500

    new test link

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,69 +17,86 @@ function fixNav() {
 }
 
 $(".tab1").click(function () {
-  $(".content1").show('slow');
+  $(".content1").show("slow");
 });
 
 $(".tab2").click(function () {
-  $(".content2").show('slow');
+  $(".content2").show("slow");
 });
 
 $(".tab3").click(function () {
-$(".content3").show('slow');
+  $(".content3").show("slow");
 });
 
 $(".tab4").click(function () {
-$(".content4").show('slow');
+  $(".content4").show("slow");
 });
 
 $(".tab5").click(function () {
-$(".content5").show('slow');
+  $(".content5").show("slow");
 });
 
-
-
 function openCity(evt, cityName, imageId) {
-    var i, tabcontent, tablinks, tabimage;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    tabimage = document.getElementsByClassName("tabimage");
-    
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-      tabimage[i].style.display = "none";
-    }
+  var i, tabcontent, tablinks, tabimage;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  tabimage = document.getElementsByClassName("tabimage");
 
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    document.getElementById(imageId).style.display = "block";
-    evt.currentTarget.className += " active";
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+    tabimage[i].style.display = "none";
   }
 
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  document.getElementById(imageId).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
-  function openGallery(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-      tabimage[i].style.display = "none";
-    }
+function openGallery(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
 
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+    tabimage[i].style.display = "none";
   }
 
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
 
-  function openDrop(event) {
-    event.preventDefault();
-    console.log("menu open")
-    var drop = document.getElementById("filter_list");
-    drop.style.display = "block";
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function openDrop(event) {
+  event.preventDefault();
+  console.log("menu open");
+  var drop = document.getElementById("filter_list");
+  drop.style.display = "block";
+}
+
+function galary(event) {
+  event.preventDefault();
+  if (event.target.value === "domotique") {
+    $("#sureteGallery").hide("slow");
+    $("#videosurveillanceGallery").hide("slow");
+    $("#domotiqueGallery").show("slow");
+  } else if (event.target.value === "surete") {
+    $("#sureteGallery").show("slow");
+    $("#videosurveillanceGallery").hide("slow");
+    $("#domotiqueGallery").hide("slow");
+  } else if (event.target.value === "videosurveillance") {
+    $("#sureteGallery").hide("slow");
+    $("#videosurveillanceGallery").show("slow");
+    $("#domotiqueGallery").hide("slow");
+  } else {
+    $("#sureteGallery").show("slow");
+    $("#videosurveillanceGallery").show("slow");
+    $("#domotiqueGallery").show("slow");
+  }
 }
